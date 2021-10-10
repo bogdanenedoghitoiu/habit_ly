f106021b062db76ef1ea80d9fb3b520c63a13346
Bogdan Ene-Doghitoiu <bogdanenedoghitoiu@gmail.com>
2021-10-10 18:50:13 +0300

    Firebase FlutterFire initialization
    - added FlutterFire initialization
    - added google services firebase json
    - modified the android build files

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_ly/ui/welcome/welcome_screen.dart';
@@ -8,23 +9,93 @@ import 'configuration/dependency_injection.dart';
 void main() {
   configureDependencies();
   Bloc.observer = DefaultBlocObserver();
+
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// TODO bloc architecture
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _firebaseInitialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Habit_ly',
-        theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          appBarTheme: AppBarTheme(color: Colors.white, elevation: 0),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home:  getIt.get<WelcomeScreen>(),
-      ),
+    return FutureBuilder(
+      future: _firebaseInitialization,
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasError) {
+          return Container(
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Habit_ly',
+              theme: ThemeData(
+                scaffoldBackgroundColor: Colors.white,
+                appBarTheme: AppBarTheme(color: Colors.white, elevation: 0),
+                visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
+              home: Scaffold(
+                body: Text("Error."),
+              ),
+            ),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Container(
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Habit_ly',
+              theme: ThemeData(
+                scaffoldBackgroundColor: Colors.white,
+                appBarTheme: AppBarTheme(color: Colors.white, elevation: 0),
+                visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
+              home: getIt.get<WelcomeScreen>(),
+            ),
+          );
+        }
+
+        return Container(
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Habit_ly',
+            theme: ThemeData(
+              scaffoldBackgroundColor: Colors.white,
+              appBarTheme: AppBarTheme(color: Colors.white, elevation: 0),
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: Scaffold(
+              body: Text("Loading."),
+            ),
+          ),
+        );
+      },
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: 'Habit_ly',
+//         theme: ThemeData(
+//           scaffoldBackgroundColor: Colors.white,
+//           appBarTheme: AppBarTheme(color: Colors.white, elevation: 0),
+//           visualDensity: VisualDensity.adaptivePlatformDensity,
+//         ),
+//         home:  getIt.get<WelcomeScreen>(),
+//       ),
+//     );
+//   }
+// }
